622e62b262b2bace6efbc8ab39a11dfb4e666fa5
vsolo <vsolo0@gmail.com>
2021-08-15 18:56:03 +0300

    0.00

---
+++ Assets/Scripts/LightControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightControl : MonoBehaviour{
+    public GameObject red;
+    public GameObject yellow;
+    public GameObject green;
+    private bool _colorRed = true;
+    private bool _colorYellow;
+    private bool _colorGreen;
+
+    public void SelectRed(){
+        if (this._colorRed){
+            this.red.GetComponent<Renderer>().material.color = new Color(0, 0, 0);
+            this._colorRed = false;
+        }
+        else{
+            this.red.GetComponent<Renderer>().material.color = new Color(1f, 0, 0);
+            this._colorRed = true;
+        }
+    }
+
+    public void SelectYellow(){
+        if (this._colorYellow){
+            this.yellow.GetComponent<Renderer>().material.color = new Color(0, 0, 0);
+            this._colorYellow = false;
+        }
+        else{
+            this.yellow.GetComponent<Renderer>().material.color = new Color(1f, 1f, 0);
+            this._colorYellow = true;
+        }
+    }
+
+    public void SelectGreen(){
+        if (this._colorGreen){
+            this.green.GetComponent<Renderer>().material.color = new Color(0, 0, 0);
+            this._colorGreen = false;
+        }
+        else{
+            this.green.GetComponent<Renderer>().material.color = new Color(0, 1f, 0);
+            this._colorGreen = true;
+        }
+    }
+}
---
+++ Assets/Scripts/TrafficLight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrafficLight : MonoBehaviour
+{
+    public GameObject  box;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Debug.Log("Test from solo start");
+        // if (this.box == null){
+        //     Debug.Log("Box is null");
+        // }
+        //box.SetActive(false);
+        Destroy(box.GetComponent<BoxCollider>());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Debug.Log("Я изучаю Unity!");
+    }
+}
